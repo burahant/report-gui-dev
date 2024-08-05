b3089e476ff34c97fff130a1df674d35c94d88b3
burahant <burahant@tgs-enterprise.com>
2024-08-05 16:53:54 +0700

    setup nes.css, add logo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="#004aad" dark>
+      <div class="d-flex align-center">
+        <v-img alt="Report GUI Logo" class="shrink mr-2" contain src="./assets/report-gui-nobackground.png" transition="scale-transition" width="200" height="65" />
+        <!-- <span>Report GUI 1.0</span> -->
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <div class="nes-text league-spartan mr-2">
+        <v-icon>mdi-github</v-icon>
+        <span> Github</span>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.press-start-2p-regular {
+  font-family: 'Press Start 2P', system-ui;
+  font-weight: 400;
+  font-style: normal;
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.league-spartan {
+  font-family: 'League Spartan', sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
 }
 </style>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  })
+}
+</script>
